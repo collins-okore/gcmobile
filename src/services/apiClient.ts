26b90797023e015d3db849462f1bc2e3f9149553
Collins Okore <okorecollins2010@gmail.com>
2025-12-09 20:00:21 +0300

    Update apiClient.ts

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -8,8 +8,8 @@ const getBaseURL = () => {
     return process.env.API_BASE_URL; // Production/custom environment
   }
   // return 'https://a5e2a068b62d.ngrok-free.app/api';
-  return 'https://api.gateconnect.io/api';
-  // return 'http://192.168.100.44:1337/api';
+  // return 'https://api.gateconnect.io/api';
+  return 'http://192.168.100.45:1337/api';
 
   // Development URLs
   // if (Platform.OS === 'android') {
